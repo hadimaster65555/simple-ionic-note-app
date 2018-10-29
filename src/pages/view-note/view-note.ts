04acfb7f1f9045ae2261a6dc25347516cc6140f5
qti <raden.muhammad.hadi@qti.co.id>
2018-10-29 21:48:08 +0700

    first commit

---
+++ src/pages/view-note/view-note.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NoteServiceProvider } from '../../providers/note-service/note-service';
+import { Note } from '../../models/note.model';
+
+@IonicPage()
+@Component({
+  selector: 'page-view-note',
+  templateUrl: 'view-note.html',
+})
+export class ViewNotePage {
+
+  note: Note;
+
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    private noteService: NoteServiceProvider) {
+      this.note = this.navParams.get('note');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ViewNotePage');
+  }
+
+  deleteNote(createDate:number) {
+    this.noteService.deleteNote(createDate);
+    this.navCtrl.pop();
+  }
+
+}
